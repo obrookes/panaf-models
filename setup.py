2a47e47f70e5489bbc91cf4978171483e338df37
obrookes <dl18206@bristol.ac.uk>
2022-05-20 13:17:57 +0100

    package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="src",
+    version="0.0.1",
+    author="Otto Brookes",
+    description="A Package for Training PanAf Models",
+    packages=setuptools.find_packages(),
+)
