08d6df3776400b1b98e463f1f1995d159f089d98
obrookes <dl18206@bristol.ac.uk>
2022-05-25 11:42:16 +0100

    add temporal subsample

--- src/supervised/modules/slowfast_r50/train_slowfast.py
+++ src/supervised/modules/slowfast_r50/train_slowfast.py
@@ -7,7 +7,6 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 from panaf.datamodules import SupervisedPanAfDataModule
 from src.supervised.models import SlowFast50
-from pytorchvideo.transforms.functional import uniform_temporal_subsample
 
 
 class ActionClassifier(pl.LightningModule):
@@ -27,7 +26,7 @@ class ActionClassifier(pl.LightningModule):
 
         x, y = batch
         slow = x["spatial_sample"].permute(0, 2, 1, 3, 4)
-        fast = uniform_temporal_subsample(slow, 8, 2)
+        fast = self.uniform_temporal_subsample(x=slow, temporal_dim=2, num_samples=8)
 
         pred = self.slowfast([fast, slow])
         loss = F.cross_entropy(pred, y)
@@ -70,14 +69,7 @@ class ActionClassifier(pl.LightningModule):
 
         x, y = batch
         slow = x["spatial_sample"].permute(0, 2, 1, 3, 4)
-
-        t = slow.shape[2]
-        assert 8 > 0 and t > 0
-        # Sample by nearest neighbor interpolation if num_samples > t.
-        indices = torch.linspace(0, t - 1, 8)
-        indices = torch.clamp(indices, 0, t - 1).long()
-        fast = torch.index_select(slow, 2, indices)
-
+        fast = self.uniform_temporal_subsample(x=slow, temporal_dim=2, num_samples=8)
         pred = self.slowfast([fast, slow])
         loss = F.cross_entropy(pred, y)
         top1_val_acc = self.top1_val_accuracy(pred, y)
@@ -124,6 +116,15 @@ class ActionClassifier(pl.LightningModule):
         )
         return optimizer
 
+    def uniform_temporal_subsample(self, x, temporal_dim, num_samples):
+        t = x.shape[temporal_dim]
+        assert num_samples > 0 and t > 0
+        # Sample by nearest neighbor interpolation if num_samples > t.
+        indices = torch.linspace(0, t - 1, num_samples, device=self.device)
+        indices = torch.clamp(indices, 0, t - 1).long()
+        fast = torch.index_select(x, temporal_dim, indices)
+        return fast
+
 
 def main():
 
@@ -152,7 +153,11 @@ def main():
         )
     else:
         trainer = pl.Trainer(
-            max_epochs=cfg.getint("trainer", "max_epochs"), fast_dev_run=5
+            gpus=cfg.getint("trainer", "gpus"),
+            num_nodes=cfg.getint("trainer", "num_nodes"),
+            max_epochs=cfg.getint("trainer", "max_epochs"),
+            stochastic_weight_avg=cfg.getboolean("trainer", "swa"),
+            fast_dev_run=5,
         )
     trainer.fit(model=model, datamodule=data_module)
 
