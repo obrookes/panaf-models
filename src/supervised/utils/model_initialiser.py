0921e091ccafaf2deb5d44fa37e32c79bf918afa
obrookes <dl18206@bristol.ac.uk>
2022-07-13 15:32:05 +0100

    add out dims to models as arg

--- src/supervised/utils/model_initialiser.py
+++ src/supervised/utils/model_initialiser.py
@@ -1,20 +1,26 @@
 from src.supervised.models import (
-    ResNet50S, # single rgb-stream
-    RGBDenseNetworkSF, # dual rgb and pose
-    RGBFlowNetworkSF, # dual rgb + flow
-    ThreeStreamNetworkSF # triple-stream
+    ResNet50S,  # single rgb-stream
+    RGBDenseNetworkSF,  # dual rgb and pose
+    RGBFlowNetworkSF,  # dual rgb + flow
+    ThreeStreamNetworkSF,  # triple-stream
 )
 
-def initialise_model(name, freeze_backbone):
-    if name == 'r':
-        model = ResNet50S(freeze_backbone=freeze_backbone)
-    elif name == 'rd':
-        model = RGBDenseNetworkSF(freeze_backbone=freeze_backbone)
-    elif name == 'rf':
-        model = RGBFlowNetworkSF(freeze_backbone=freeze_backbone)
-    elif name == 'rdf':
-        model = ThreeStreamNetworkSF(freeze_backbone=freeze_backbone)
+
+def initialise_model(name, freeze_backbone, out_features=9):
+    if name == "r":
+        model = ResNet50S(freeze_backbone=freeze_backbone, out_features=out_features)
+    elif name == "rd":
+        model = RGBDenseNetworkSF(
+            freeze_backbone=freeze_backbone, out_features=out_features
+        )
+    elif name == "rf":
+        model = RGBFlowNetworkSF(
+            freeze_backbone=freeze_backbone, out_features=out_features
+        )
+    elif name == "rdf":
+        model = ThreeStreamNetworkSF(
+            freeze_backbone=freeze_backbone, out_features=out_features
+        )
     else:
         raise NameError(f"The model initialisation: {name} does not exist!")
     return model
-
