513bb716b35a015daee9c65cab2f6fc4b3426860
obrookes <dl18206@bristol.ac.uk>
2022-07-13 09:16:20 +0100

    util to load correct model by name

---
+++ src/supervised/utils/model_initialiser.py
@@ -0,0 +1,20 @@
+from src.supervised.models import (
+    ResNet50, # single rgb-stream
+    RGBDenseNetworkSF, # dual rgb and pose
+    RGBFlowNetworkSF, # dual rgb + flow
+    ThreeStreamNetworkSF # triple-stream
+)
+
+def initialise_model(name, freeze_backbone):
+    if name == 'r':
+        model = ResNet50(freeze_backbone=freeze_backbone)
+    elif name == 'rd':
+        model = RGBDenseNetworkSF(freeze_backbone=freeze_backbone)
+    elif name == 'rf':
+        model = RGBFlowNetworkSF(freeze_backbone=freeze_backbone)
+    elif name == 'rdf':
+        model = ThreeStreamNetworkSF(freeze_backbone=freeze_backbone)
+    else:
+        raise NameError(f"The model initialisation: {name} does not exist!")
+    return model
+
