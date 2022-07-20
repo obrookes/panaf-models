a96fff12387249ef0e2595f59b81b55222ebb1bd
obrookes <dl18206@bristol.ac.uk>
2022-07-20 11:55:21 +0100

    modified to include r101 backbone

--- src/supervised/models/slowfast.py
+++ src/supervised/models/slowfast.py
@@ -1,16 +1,26 @@
 import torch
 from torch import nn
-from torch.nn import Conv3d
 from pytorchvideo.models.head import create_res_basic_head
 
 
-class SlowFast50(nn.Module):
-    def __init__(self):
+class SlowFast(nn.Module):
+    def __init__(self, model_name="slowfast_r50"):
+
+        """
+        Other models:
+        'name: slowfast_16x8_r101_50_50'
+        'input: slow = torch.rand(1, 3, 64, 244, 244)
+                fast = torch.rand(1, 3, 16, 244, 244)
+
+        'name: slowfast_r101
+        'input: slow = torch.rand(1, 3, 32, 244, 244)
+                fast = torch.rand(1, 3, 8, 244, 244)'
+        """
 
         super().__init__()
 
         slowfast = torch.hub.load(
-            "facebookresearch/pytorchvideo:main", model="slowfast_r50", pretrained=True
+            "facebookresearch/pytorchvideo:main", model=model_name, pretrained=True
         )
         self.backbone = nn.Sequential(*list(slowfast.children())[0][:-1])
         self.res_head = create_res_basic_head(
